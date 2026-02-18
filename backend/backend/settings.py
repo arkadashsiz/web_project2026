bde9ade5332cdc528351419754d0c72d2e6b60b7
Reza <r.eslami.1383@gmail.com>
2026-02-18 10:11:52 +0330

    models in users app are created

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,4 +135,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+    
+    # 100 = Chief, 80 = Captain, 60 = Sergeant, 40 = Detective, 20 = Officer, 10 = Cadet
+    access_level = models.PositiveIntegerField(
+        default=10, 
+        help_text="Higher number = Higher Authority. Used for permission checks."
+    )
+
+    def __str__(self):
+        return f"{self.name} (Level {self.access_level})"
+    
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    national_id = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True, blank=True)
+    
+    # Link to the Dynamic Role
+    role = models.ForeignKey(
+        Role, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        related_name="personnel"
+    )
+
+    REQUIRED_FIELDS=[
+        "email",
+        "phone_number",
+        "national_id",
+        "first_name",
+        "last_name",
+    ]
+
+    def __str__(self):
+        role_name = self.role.name if self.role else "Civilian/Unassigned"
+        return f"{self.last_name}, {self.first_name} - {role_name}"
